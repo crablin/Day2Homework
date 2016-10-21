c12131d610768dcb33450a38dcc72c25e27aff2d
Crab Lin <lin.crab@gmail.com>
2016-10-21 22:12:39 +0800

    let scenario2 pass

--- Day2Homework/PotterShoppingCart.cs
+++ Day2Homework/PotterShoppingCart.cs
@@ -8,24 +8,54 @@ namespace Day2Homework
     {
         private List<PotterBook> list;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PotterShoppingCart"/> class.
+        /// </summary>
         public PotterShoppingCart()
         {
             this.list = new List<PotterBook>();
         }
 
+        /// <summary>
+        /// Adds the specified book.
+        /// </summary>
+        /// <param name="book">The book.</param>
         public void Add(PotterBook book)
         {
             this.list.Add(book);
         }
-        
-        public object GetTotal()
-        {
-            return this.list.Sum(b => b.Price);
-        }
 
+        /// <summary>
+        /// Adds the specified books.
+        /// </summary>
+        /// <param name="books">The books.</param>
         public void Add(List<PotterBook> books)
         {
-            throw new NotImplementedException();
+            this.list.AddRange(books);
         }
+
+        /// <summary>
+        /// Gets the total.
+        /// </summary>
+        /// <returns></returns>
+        public double GetTotal()
+        {
+            var rate = GetDiscountRate();
+
+            return this.list.Sum(b => b.Price) * rate;
+        }
+
+        /// <summary>
+        /// Gets the discount rate.
+        /// </summary>
+        /// <returns></returns>
+        private double GetDiscountRate()
+        {
+            if (this.list.Count == 2)
+                return 0.95;
+
+            return 1;
+        }
+
     }
 }
