1af544dadb076a9bf6b2b6f229b52caef9af8b0e
Crab Lin <lin.crab@gmail.com>
2016-10-21 20:30:03 +0800

    init Day2Homework

---
+++ Day2HomeworkTests/PotterShoppingCartTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day2HomeworkTests
+{
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+        /// <summary>
+        /// 第一集買了一本，其他都沒買，價格應為100*1=100元
+        /// </summary>
+        [TestMethod]
+        public void 第一集買了一本_價格應為100()
+        {
+            Assert.Fail();
+        }
+
+        
+    }
+}
