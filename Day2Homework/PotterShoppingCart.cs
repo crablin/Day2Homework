47c019a3929241c730593e7c0c9084b681e0747a
Crab Lin <lin.crab@gmail.com>
2016-10-24 09:49:16 +0800

    add marks on package total discounts algorithm

--- Day2Homework/PotterShoppingCart.cs
+++ Day2Homework/PotterShoppingCart.cs
@@ -64,17 +64,21 @@ namespace Day2Homework
         private IEnumerable<double> GetPackageTotalDiscounts()
         {
             var prevQuantity = 0;
+            // 取得需計算的數列並排序 ex. [1, 200, 200, 3, 4] => [1, 3, 4, 200]
             var packageQuantities = this.list.Select(b => b.Quantity).Distinct().OrderBy(b => b);
 
+            // 依數列計算每個數量的價格 loop n = 1 ~ 5
             foreach (var packageQuantity in packageQuantities)
             {
+                // 取得指定數量的書本清單
                 var packageBooks = this.list.Where(b => b.Quantity >= packageQuantity);
 
                 var sum = packageBooks.Sum(b => b.Price);
                 var episodeCount = packageBooks.Count();
                 var rate = discountRates[episodeCount];
                 var packageRange = packageQuantity - prevQuantity;
-
+                
+                // 回傳此數量計算折扣價格：書本總價 x 依購買集數取得折扣 x 書本套數
                 yield return sum * rate * packageRange;
                 prevQuantity = packageQuantity;
             }
